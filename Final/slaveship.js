3740d3aedcf3c75a1698ccf1cfc6c34b8911a400
Robin Reid <robinreid3@gmail.com>
2012-12-07 11:53:06 -0500

    first commit final

---
+++ Final/slaveship.js
@@ -0,0 +1,6 @@
+{
+
+document.getElementById("jname").innerHTML=JSONObject.name  
+document.getElementById("jage").innerHTML=JSONObject.age  
+document.getElementById("jstreet").innerHTML=JSONObject.street  
+document.getElementById("jphone").innerHTML=JSONObject.phone  
